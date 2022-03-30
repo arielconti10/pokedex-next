613596b5c463ab603c9d6e7086c57c6bbcc8feaf
Ariel Conti <arielconti10@gmail.com>
2022-03-30 16:56:48 +0100

    feat: added footer

--- src/layouts/Base/index.tsx
+++ src/layouts/Base/index.tsx
@@ -15,12 +15,25 @@ const BaseLayout = ({ children, ...props }: BaseLayoutProps) => {
       justifyContent="flex-start"
       alignItems="center"
       flexDirection="column"
+      position="relative"
       {...props}
     >
       <Header>
         <Navigation />
       </Header>
       {children}
+      <x.div position="absolute" bottom="10" fontWeight="medium">
+        Made with ❤️ by{' '}
+        <x.a
+          href="https://github.com/arielconti10"
+          target="_blank"
+          color="link"
+          rel="norefer noopener"
+          textDecoration={{ hover: 'underline' }}
+        >
+          Ariel Conti
+        </x.a>
+      </x.div>
     </x.main>
   )
 }
