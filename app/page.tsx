76a2ab6346c2268511cd9be906510960be7b71a3
Ariel Conti <arielconti10@gmail.com>
2023-05-04 08:23:24 +0100

    debugging vercel

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,14 @@ const getBaseUrl = cache(() =>
 );
 
 async function getData() {
-  const res = await fetch(`${getBaseUrl()}/api/pokemon`)
-  if (!res.ok) {
-    throw new Error("Failed to fetch data")
-  }
+  console.log(getBaseUrl())
+  return getBaseUrl()
+  // const res = await fetch(`${getBaseUrl()}/api/pokemon`)
+  // if (!res.ok) {
+  //   throw new Error("Failed to fetch data")
+  // }
 
-  return res.json()
+  // return res.json()
 }
 
 export default async function IndexPage() {
@@ -47,7 +49,7 @@ export default async function IndexPage() {
         </div>
 
         <div className="space-4 mt-4 flex w-full flex-row flex-wrap justify-between gap-4">
-          {data && data.map((pokemon) => (
+          {/* {data && data.map((pokemon) => (
             <Card key={data.id} className="flex flex-col items-center">
               <CardContent>
                 <Image
@@ -61,7 +63,7 @@ export default async function IndexPage() {
                 <CardTitle>{pokemon.identifier}</CardTitle>
               </CardFooter>
             </Card>
-          ))}
+          ))} */}
         </div>
 
         {/* <Search /> */}
