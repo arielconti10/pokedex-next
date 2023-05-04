de23d3b64642f15459e798c1287f31b1840c559c
Ariel Conti <arielconti10@gmail.com>
2023-05-04 19:41:27 +0100

    added getbaseurl util

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,12 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { cache } from 'react';
+
+export const getBaseUrl = cache(() =>
+  process.env.VERCEL_URL
+    ? `https://pokedex-next-tau.vercel.app`
+    : `http://localhost:${process.env.PORT ?? 3000}`,
+);
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
